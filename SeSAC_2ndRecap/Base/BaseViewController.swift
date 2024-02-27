fe7e22e7e10e50fe10641f91b2289c2fbda2dca0
jieun <jieun_0330@naver.com>
2024-02-27 16:00:57 +0900

    :lipstick: [style] 코인검색화면 UI 구현

--- SeSAC_2ndRecap/Base/BaseViewController.swift
+++ SeSAC_2ndRecap/Base/BaseViewController.swift
@@ -12,6 +12,10 @@ class BaseViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        configureHierarchy()
+        configureConstraints()
+        configureView()
+        
     }
 
     func configureHierarchy() { }
--- SeSAC_2ndRecap/Search/SearchViewController.swift
+++ SeSAC_2ndRecap/Search/SearchViewController.swift
@@ -10,14 +10,28 @@ import Then
 import SnapKit
 
 class SearchViewController: BaseViewController {
-    
+
     let profileImage = UIImageView().then {
         $0.image = .tabUser
-//        $0.layer.borderColor = 
+        $0.layer.borderColor = DesignSystemColor.purple.value.cgColor
+        $0.layer.borderWidth = 2
     }
     
     let mainTitle = UILabel().then {
         $0.text = "Search"
+        $0.font = UIFont.boldSystemFont(ofSize: 30)
+    }
+    
+    let searchBar = UISearchBar().then {
+        $0.placeholder = "코인명 검색"
+        $0.backgroundImage = UIImage()
+        $0.layer.cornerRadius = 10
+    }
+    
+    lazy var tableView = UITableView().then {
+//        $0.delegate = self
+//        $0.dataSource = self
+        $0.backgroundColor = .orange
     }
 
     override func viewDidLoad() {
@@ -26,7 +40,7 @@ class SearchViewController: BaseViewController {
     }
     
     override func configureHierarchy() {
-        [profileImage, mainTitle].forEach {
+        [profileImage, mainTitle, searchBar, tableView].forEach {
             view.addSubview($0)
         }
     }
@@ -35,7 +49,25 @@ class SearchViewController: BaseViewController {
         profileImage.snp.makeConstraints {
             $0.top.equalTo(view.safeAreaLayoutGuide)
             $0.trailing.equalToSuperview().inset(20)
-            $0.size.equalTo(30)
+            $0.size.equalTo(35)
+        }
+        
+        mainTitle.snp.makeConstraints {
+            $0.top.equalTo(profileImage.snp.bottom).offset(10)
+            $0.horizontalEdges.equalToSuperview().inset(20)
+            $0.height.equalTo(30)
+        }
+        
+        searchBar.snp.makeConstraints {
+            $0.horizontalEdges.equalToSuperview().inset(15)
+            $0.height.equalTo(35)
+            $0.top.equalTo(mainTitle.snp.bottom).offset(10)
+        }
+        
+        tableView.snp.makeConstraints {
+            $0.horizontalEdges.equalToSuperview().inset(25)
+            $0.top.equalTo(searchBar.snp.bottom).offset(10)
+            $0.bottom.equalTo(view.safeAreaLayoutGuide)
         }
     }
     
@@ -44,3 +76,14 @@ class SearchViewController: BaseViewController {
     }
 
 }
+
+//extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
+//    
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return 1
+//    }
+//    
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        <#code#>
+//    }
+//}
