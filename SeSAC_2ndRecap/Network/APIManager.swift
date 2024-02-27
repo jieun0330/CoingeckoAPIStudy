d0cc4c134187eb41c14b880eb676c386dccc7f62
jieun <jieun_0330@naver.com>
2024-02-27 16:15:19 +0900

    :sparkles: [feat] Alamofire 라이브러리 설정

---
+++ SeSAC_2ndRecap/Network/APIManager.swift
@@ -0,0 +1,8 @@
+//
+//  APIManager.swift
+//  SeSAC_2ndRecap
+//
+//  Created by 박지은 on 2/27/24.
+//
+
+import Foundation
--- SeSAC_2ndRecap/Search/SearchTableViewCell.swift
+++ SeSAC_2ndRecap/Search/SearchTableViewCell.swift
@@ -7,17 +7,32 @@
 
 import UIKit
 
-class SearchTableViewCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+class SearchTableViewCell: BaseTableViewCell {
+    
+    let icon = UIImageView().then { <#UIImageView#> in
+        <#code#>
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super .init(style: style, reuseIdentifier: reuseIdentifier)
     }
-
+    
+    override func configureHierarchy() {
+        [icon].forEach {
+            contentView.addSubview($0)
+        }
+    }
+    
+    override func configureConstraints() {
+        <#code#>
+    }
+    
+    override func configureView() {
+        <#code#>
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 }
