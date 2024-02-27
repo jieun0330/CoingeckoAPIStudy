ba549ec11aed85742ac0d64835ee002d0436288a
jieun <jieun_0330@naver.com>
2024-02-27 15:04:38 +0900

    :sparkles: [feat] BaseViewController 생성

---
+++ SeSAC_2ndRecap/Base/BaseViewController.swift
@@ -0,0 +1,20 @@
+//
+//  BaseViewController.swift
+//  SeSAC_2ndRecap
+//
+//  Created by 박지은 on 2/27/24.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    func configureHierarchy() { }
+    func configureConstraints() { }
+    func configureView() { }
+}
