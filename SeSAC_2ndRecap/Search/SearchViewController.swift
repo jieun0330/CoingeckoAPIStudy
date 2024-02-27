9fce407f12fb025ba6bb50216b40c2dc9aabb496
jieun <jieun_0330@naver.com>
2024-02-27 15:42:49 +0900

    :lipstick: [style] DesignSystem Colors 추가

--- SeSAC_2ndRecap/Search/SearchViewController.swift
+++ SeSAC_2ndRecap/Search/SearchViewController.swift
@@ -7,11 +7,17 @@
 
 import UIKit
 import Then
+import SnapKit
 
 class SearchViewController: BaseViewController {
     
-    let profileImage = UIImageView().then { <#UIImageView#> in
-        <#code#>
+    let profileImage = UIImageView().then {
+        $0.image = .tabUser
+//        $0.layer.borderColor = 
+    }
+    
+    let mainTitle = UILabel().then {
+        $0.text = "Search"
     }
 
     override func viewDidLoad() {
@@ -20,15 +26,21 @@ class SearchViewController: BaseViewController {
     }
     
     override func configureHierarchy() {
-        <#code#>
+        [profileImage, mainTitle].forEach {
+            view.addSubview($0)
+        }
     }
     
     override func configureConstraints() {
-        <#code#>
+        profileImage.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.trailing.equalToSuperview().inset(20)
+            $0.size.equalTo(30)
+        }
     }
     
     override func configureView() {
-        <#code#>
+        view.backgroundColor = .white
     }
 
 }
