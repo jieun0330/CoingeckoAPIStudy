a171867b2f11595dc8dda1bf65dfafd1f2fb3876
jieun <jieun_0330@naver.com>
2024-02-28 08:56:11 +0900

    :lipstick: [style] 탭바 구현

--- SeSAC_2ndRecap/SceneDelegate.swift
+++ SeSAC_2ndRecap/SceneDelegate.swift
@@ -13,15 +13,24 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        let tabBarController = UITabBarController()
+        
+//        let trendingView =
+        let searchView = SearchViewController()
+//        let favoritesView =
+//        let chartView =
+        
+        window?.rootViewController = tabBarController
+        tabBarController.setViewControllers([searchView], animated: true)
+        
+        searchView.tabBarItem = UITabBarItem(title: "", image: UIImage(systemName: "magnifyingglass"), tag: 1)
+        
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let scene = (scene as? UIWindowScene) else { return }
+        guard let _ = (scene as? UIWindowScene) else { return }
         
-        window = UIWindow(windowScene: scene)
-        
-        window?.rootViewController = SearchViewController()
-        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
